1e19b254efcba00c4147310f995e5edbd4060f21
Morten Peter Hagh Jensen <mortenpeterhaghjensen@Mortens-MBP-3.localdomain>
2020-02-01 11:45:03 +0100

    Add confirmation page

---
+++ pages/contact/confirmation.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="confirmation">
+    <div class="container">
+      <h1>Thank you</h1>
+
+      <p>
+        Your message was recieved! We will get back to you as soon as possible
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
